f8e876b05c8eb177f808d57412a807fc54bb20ae
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-09-05 14:38:54 +0200

    Update TrainingMapper.js

--- Frontend/src/services/mapper/TrainingMapper.js
+++ Frontend/src/services/mapper/TrainingMapper.js
@@ -1,67 +1,65 @@
-import get from "lodash/get";
-import uniqueId from "lodash/uniqueId";
-import { training } from "@/tailwind/Colors";
+import get from 'lodash/get';
+import uniqueId from 'lodash/uniqueId';
+import { training } from '@/tailwind/Colors';
 
 export default class TrainingMapper {
   static this(data) {
     return {
-      id: get(data, "id", ""),
-      slug: get(data, "slug", ""),
-      title: get(data, "title.rendered", ""),
-      informations: get(data, "acf.informations", {}),
-      categories: get(data, "training-category", []),
-      pghealth: get(data, "pghealth", 0),
-      product: get(data, "product", 0),
-      trainings: get(data, "trainings", []).map(training =>
-        TrainingMapper.mapTrainingInSeries(training)
-      ),
+      id: get(data, 'id', ''),
+      slug: get(data, 'slug', ''),
+      title: get(data, 'title.rendered', ''),
+      informations: get(data, 'acf.informations', {}),
+      categories: get(data, 'training-category', []),
+      pghealth: get(data, 'pghealth', 0),
+      product: get(data, 'product', 0),
+      trainings: (get(data, 'trainings') || []).map(training => TrainingMapper.mapTrainingInSeries(training)),
       /* TrainingMapper.sortTrainings(
         //  data
         get(data, "trainings", []).map(training => TrainingMapper.mapTrainingInSeries(training))
       ), */
-      summaryPage: get(data, "acf.summary_page", {}),
-      dutyText: get(data, "acf.duty_text", ""),
-      boost: get(data, "acf.informations.boost", ""),
-      finishPage: get(data, "acf.finish_page.success_page", {}),
-      related: get(data, "acf.related_trainings", {})
+      summaryPage: get(data, 'acf.summary_page', {}),
+      dutyText: get(data, 'acf.duty_text', ''),
+      boost: get(data, 'acf.informations.boost', ''),
+      finishPage: get(data, 'acf.finish_page.success_page', {}),
+      related: get(data, 'acf.related_trainings', {}),
     };
   }
 
   static mapTrainingInSeries(data) {
-    const lessons = get(data, "lessons", []).map(lesson => ({
-      trainingSeriesId: get(data, "training_series_id", null),
-      training_id: get(data, "training.ID", null),
-      ...TrainingMapper.mapLesson(lesson.lesson)
+    const lessons = get(data, 'lessons', []).map(lesson => ({
+      trainingSeriesId: get(data, 'training_series_id', null),
+      training_id: get(data, 'training.ID', null),
+      ...TrainingMapper.mapLesson(lesson.lesson),
     }));
 
     return {
-      trainingSeriesId: get(data, "training_series_id", null),
-      id: get(data, "training.ID", null),
-      slug: get(data, "training.post_name", null),
-      globals: get(data, "globals", {}),
+      trainingSeriesId: get(data, 'training_series_id', null),
+      id: get(data, 'training.ID', null),
+      slug: get(data, 'training.post_name', null),
+      globals: get(data, 'globals', {}),
       isPremium: data.training.is_premium,
-      year: get(data, "year", ""),
+      year: get(data, 'year', ''),
       lessons,
-      storeEndpoint: get(data, "storeEndpoint", false)
+      storeEndpoint: get(data, 'storeEndpoint', false),
     };
   }
 
   static mapChapter(chapter) {
     return {
-      id: get(chapter, "chapter_id", uniqueId("tq-")),
-      chapter_id: get(chapter, "chapter_id", uniqueId("tq-")),
-      image: get(chapter, "image", {}),
-      question: get(chapter, "question", ""),
-      choices: get(chapter, "choices", []).map(choice => TrainingMapper.mapChoice(choice)),
-      references: get(chapter, "references", "")
+      id: get(chapter, 'chapter_id', uniqueId('tq-')),
+      chapter_id: get(chapter, 'chapter_id', uniqueId('tq-')),
+      image: get(chapter, 'image', {}),
+      question: get(chapter, 'question', ''),
+      choices: get(chapter, 'choices', []).map(choice => TrainingMapper.mapChoice(choice)),
+      references: get(chapter, 'references', ''),
     };
   }
 
   static mapChoice(choice) {
     return {
-      id: get(choice, "choice_id", uniqueId("choice-")),
+      id: get(choice, 'choice_id', uniqueId('choice-')),
       ...choice,
-      validationClass: null
+      validationClass: null,
     };
   }
 
@@ -70,9 +68,9 @@ export default class TrainingMapper {
       ...lesson,
       quiz: {
         ...lesson.quiz,
-        chapters: lesson.quiz.chapters.map(chapter => TrainingMapper.mapChapter(chapter))
+        chapters: lesson.quiz.chapters.map(chapter => TrainingMapper.mapChapter(chapter)),
       },
-      productInformation: TrainingMapper.mapProductInformation(lesson)
+      productInformation: TrainingMapper.mapProductInformation(lesson),
     };
   }
 
@@ -88,13 +86,13 @@ export default class TrainingMapper {
   static createPayload(answers, additionalFields = {}) {
     return {
       result: [TrainingMapper.createResultPayload(answers, additionalFields)],
-      ...additionalFields
+      ...additionalFields,
     };
   }
 
   static createLikesPayload(likes = {}) {
     return {
-      result: [TrainingMapper.createResultPayload(likes)]
+      result: [TrainingMapper.createResultPayload(likes)],
     };
   }
 
@@ -105,27 +103,27 @@ export default class TrainingMapper {
       choice: {
         id: answer.choice_id,
         value: answer.value,
-        is_true: answer.is_true
-      }
+        is_true: answer.is_true,
+      },
     }));
 
     return {
       training_id,
       lesson_id,
-      answers: mappedAnswers
+      answers: mappedAnswers,
     };
   }
 
   static mapProductInformation(lesson) {
-    return (get(lesson, "product_information", []) || [])
+    return (get(lesson, 'product_information', []) || [])
       .map(content => {
         if (!content.text_media_paragraph) {
           return null;
         }
 
         return {
-          acf_fc_layout: "text-media_paragraph",
-          ...content
+          acf_fc_layout: 'text-media_paragraph',
+          ...content,
         };
       })
       .filter(content => content !== null);
