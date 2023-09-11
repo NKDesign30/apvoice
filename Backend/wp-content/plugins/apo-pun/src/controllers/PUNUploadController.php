2842520e18aa90831aa86cc701c113274b43ac14
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-09-11 13:38:01 +0200

    Update PUNUploadController.php

--- Backend/wp-content/plugins/apo-pun/src/controllers/PUNUploadController.php
+++ Backend/wp-content/plugins/apo-pun/src/controllers/PUNUploadController.php
@@ -31,36 +31,40 @@ class PUNUploadController
      */
     public function update()
     {
-        if ( !current_user_can( PUNManagerRole::MANAGE_CAPABILITY ) ) {
-            $this->redirectBack( ['errors' => [__( 'Your user role have not the necessary capability for this action', 'apo-pun' )]] );
-        }
-        
-        if ( !$this->hasValidNonce() ) {
-            $this->redirectBack( ['errors' => [__( 'Invalid nonce. Try re-submitting the form.', 'apo-pun' )]] );
+        if (!current_user_can(PUNManagerRole::MANAGE_CAPABILITY)) {
+            $this->redirectBack(['errors' => [__('Your user role have not the necessary capability for this action', 'apo-pun')]]);
         }
 
-        if ( ( $message = $this->validateUploadedFile() ) !== true ) {
-            $this->redirectBack( ['errors' => [$message]] );
+        if (!$this->hasValidNonce()) {
+            $this->redirectBack(['errors' => [__('Invalid nonce. Try re-submitting the form.', 'apo-pun')]]);
         }
 
         header('Content-Type: text/plain');
 
         $values = $this->parseUploadedFile();
 
-        if ( count($values) === 0 ) {
-            $this->redirectBack( ['infos' => [__( 'No new PUN\'s codes have been imported. Everything is up to date.', 'apo-pun' )]] );
+        if (count($values) === 0) {
+            $this->redirectBack(['infos' => [__('No new PUN\'s codes have been imported. Everything is up to date.', 'apo-pun')]]);
         }
 
         $countUpdated = 0;
         $countAdded = 0;
 
         foreach ($values as $item) {
-            if ( $pun = $this->pun->exists($item['pharmacy_unique_number']) ) {
-                if($this->pun->hasNameChanged($pun->id, $item['name'])) {
+            if ($pun = $this->pun->exists($item['pharmacy_unique_number'])) {
+                if ($this->pun->hasNameChanged($pun->id, $item['name'])) {
                     $this->pun->update(['name' => $item['name']], ['id' => $pun->id]);
                     $countUpdated++;
                 }
             } else {
+                // Hier überprüfen wir die Rolle aus der CSV und setzen die entsprechende Role_id
+                if (isset($item['Role'])) {
+                    if ($item['Role'] == 'HCP') {
+                        $item['role_id'] = 7;
+                    } elseif ($item['Role'] == 'paraHCP') {
+                        $item['role_id'] = 87;
+                    }
+                }
                 $this->pun->create($item);
                 $countAdded++;
             }
@@ -68,19 +72,19 @@ class PUNUploadController
 
         $messages = [];
 
-        if ( $countAdded > 0 ) {
-            $messages = ['infos' => [__( sprintf( '%d new PUN\'s have been added.', $countAdded ), 'apo-pun' )]];
+        if ($countAdded > 0) {
+            $messages = ['infos' => [__(sprintf('%d new PUN\'s have been added.', $countAdded), 'apo-pun')]];
         }
 
-        if ( $countUpdated > 0 ) {
-            $messages = array_merge( $messages, ['infos' => [__( sprintf( '%d PUN\'s have been updated.', $countUpdated ), 'apo-pun' )]] );
+        if ($countUpdated > 0) {
+            $messages = array_merge($messages, ['infos' => [__(sprintf('%d PUN\'s have been updated.', $countUpdated), 'apo-pun')]]);
         }
 
-        if ( count( $messages ) > 0 ) {
-            $this->redirectBack( $messages );
+        if (count($messages) > 0) {
+            $this->redirectBack($messages);
         }
 
-        $this->redirectBack( ['infos' => [__( 'No new PUN\'s have been imported. Everything is up to date.', 'apo-pun' )]] );
+        $this->redirectBack(['infos' => [__('No new PUN\'s have been imported. Everything is up to date.', 'apo-pun')]]);
     }
 
     /**
@@ -92,7 +96,7 @@ class PUNUploadController
     {
         $nonce = $_POST['apo_pun_save_settings_nonce'] ?? false;
 
-        return wp_verify_nonce( wp_unslash( $nonce ), 'apo_pun_save_settings' );
+        return wp_verify_nonce(wp_unslash($nonce), 'apo_pun_save_settings');
     }
 
     /**
@@ -102,7 +106,7 @@ class PUNUploadController
      */
     protected function parseUploadedFile()
     {
-        return ParserFactory::makeFromUpload( $_FILES[$this->file] )->parse();
+        return ParserFactory::makeFromUpload($_FILES[$this->file])->parse();
     }
 
 }
