3cf8435f2c1c479620bfa6bd3e8a998c98917445
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-09-12 11:35:01 +0200

    Update Model.php

--- Backend/wp-content/mu-plugins/libraries/Model.php
+++ Backend/wp-content/mu-plugins/libraries/Model.php
@@ -26,15 +26,15 @@ class Model
      * 
      * @return Void
      */
-	public function __construct($table) 
-	{
+    public function __construct($table)
+    {
         $this->db = DB::instance();
         $this->table = $this->db->wpdb->prefix . $table;
         $this->prefix = $this->db->wpdb->prefix;
         $this->defaultPrefix = $this->db->defaultPrefix;
     }
 
-     /**
+    /**
      * Get methods
      */
     public function show()
@@ -42,7 +42,7 @@ class Model
         return $this->db->select('SELECT * FROM ' . $this->table);
     }
 
-    public function showOne($id) 
+    public function showOne($id)
     {
         return $this->db->selectRow('SELECT * FROM ' . $this->table . ' WHERE id = ' . $id);
     }
@@ -54,27 +54,27 @@ class Model
 
     public function where($where)
     {
-        $where = implode(' AND ', array_map(function($key, $value) {
+        $where = implode(' AND ', array_map(function ($key, $value) {
             return $key . ' = "' . $value . '"';
-          }, array_keys($where), $where));
-          
+        }, array_keys($where), $where));
+
         return $this->db->select('SELECT * FROM ' . $this->table . ' WHERE ' . $where);
     }
 
     public function whereFirst($where)
     {
-        return $this->where($where)[0];
+        $result = $this->where($where);
+        return !empty($result) ? $result[0] : null;
     }
-
     /**
      * Store methods
      */
-    public function create($data) 
+    public function create($data)
     {
         $this->extractFillableFields($data);
 
         $result = $this->db->insert($this->table, (array) $data);
-        if($result) {
+        if ($result) {
             return $this->showLastEntry();
         }
         return $this->db->lastError();
@@ -85,7 +85,7 @@ class Model
         $where = $data;
         $this->extractUniqueFields($where);
 
-        if($this->whereFirst($where)) {
+        if ($this->whereFirst($where)) {
             return $this->update($data, $where);
         }
         return $this->create($data);
@@ -94,13 +94,13 @@ class Model
     /**
      * Update methods
      */
-    public function update($data, $where) 
+    public function update($data, $where)
     {
         $this->extractFillableFields($data);
 
         $this->db->update($this->table, (array) $data, (array) $where);
 
-        if(!$this->db->lastError()) {
+        if (!$this->db->lastError()) {
             return $this->whereFirst($where);
         }
         return $this->db->lastError();
@@ -111,18 +111,18 @@ class Model
      */
     public function currentUserId($userId = null)
     {
-        if(!is_null($userId)) return $userId;
+        if (!is_null($userId)) return $userId;
         return $this->userId();
     }
 
-    protected function extractFillableFields(&$data) 
+    protected function extractFillableFields(&$data)
     {
         $data = $this->extractData($data, $this->fillable);
 
         return $this;
     }
 
-    protected function extractUniqueFields(&$data) 
+    protected function extractUniqueFields(&$data)
     {
         $data = $this->extractData($data, $this->unique);
 
@@ -131,7 +131,7 @@ class Model
 
     protected function extractData($data, $identifier)
     {
-        return array_filter($data, function($key) use($identifier) {
+        return array_filter($data, function ($key) use ($identifier) {
             return in_array($key, $identifier);
         }, ARRAY_FILTER_USE_KEY);
     }
