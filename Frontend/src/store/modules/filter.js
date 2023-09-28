4733bcadbc9228268f5d7b2dc41a2ed1eac84c42
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-09-28 16:42:59 +0200

    Update filter.js

--- Frontend/src/store/modules/filter.js
+++ Frontend/src/store/modules/filter.js
@@ -1,73 +1,91 @@
 import {
-    FILTER_TRAININGS,
-    CLEAR_TRAININGS,
-    SHOW_TAB,
-    CLICK,
-    SCROLL
-  } from '../types/action-types';
-import { FILTER_TRAININGSM ,CLEAR_TRAININGSM, SHOW_TABM,CLICKM} from '../types/mutation-types';
+  FILTER_TRAININGS,
+  FILTER_CATEGORY,
+  CLEAR_TRAININGS,
+  SHOW_TAB,
+  CLICK,
+  SCROLL,
+} from '../types/action-types';
+import {
+  FILTER_TRAININGSM, FILTER_CATEGORYM, CLEAR_TRAININGSM, SHOW_TABM, CLICKM,
+} from '../types/mutation-types';
 
 
 export default {
-    state: {
-      categoriesIds: [],
-      showProductSeries: !!(window.location.hash && window.location.hash === '#products'),
-      scroll:0
-    },
-  
-    mutations: {
-    [FILTER_TRAININGSM](state,categoryId) {
+  state: {
+    categoriesIds: [],
+    categoryCategoriesIds: [],
+    showProductSeries: !!(window.location.hash && window.location.hash === '#products'),
+    scroll: 0,
+  },
+
+  mutations: {
+    [FILTER_TRAININGSM](state, categoryId) {
       if (state.categoriesIds.includes(categoryId)) {
-              const index = state.categoriesIds.indexOf(categoryId);
-              state.categoriesIds.splice(index, 1);
+        const index = state.categoriesIds.indexOf(categoryId);
+        state.categoriesIds.splice(index, 1);
       } else {
-              state.categoriesIds.push(categoryId);
+        state.categoriesIds.push(categoryId);
       }
     },
-    [CLEAR_TRAININGSM](state){
-      state.categoriesIds=[];
+    [FILTER_CATEGORYM](state, categoryId) {
+      if (state.categoryCategoriesIds.includes(categoryId)) {
+        const index = state.categoryCategoriesIds.indexOf(categoryId);
+        state.categoryCategoriesIds.splice(index, 1);
+      } else {
+        state.categoryCategoriesIds.push(categoryId);
+      }
     },
-    [SHOW_TABM](state){
-      state.showProductSeries=true;
-      state.scroll=1
+    [CLEAR_TRAININGSM](state) {
+      state.categoriesIds = [];
     },
-    [CLICKM](state){
-      state.showProductSeries=!state.showProductSeries;
-    }
+    [SHOW_TABM](state) {
+      state.showProductSeries = true;
+      state.scroll = 1;
     },
-  
-    actions: {
-      
-        [FILTER_TRAININGS]({ commit }, categoryId) {
-            commit(FILTER_TRAININGSM, categoryId)
-        },
+    [CLICKM](state) {
+      state.showProductSeries = !state.showProductSeries;
+    },
+  },
 
-        [CLEAR_TRAININGS]({ commit }) {
-          commit(CLEAR_TRAININGSM)
-      },
+  actions: {
 
-      [SHOW_TAB]({ commit }) {
-        commit(SHOW_TABM)
+    [FILTER_TRAININGS]({ commit }, categoryId) {
+      commit(FILTER_TRAININGSM, categoryId);
+    },
+
+    [FILTER_CATEGORY]({ commit }, categoryId) {
+      commit(FILTER_CATEGORYM, categoryId);
+    },
+
+    [CLEAR_TRAININGS]({ commit }) {
+      commit(CLEAR_TRAININGSM);
+    },
+
+    [SHOW_TAB]({ commit }) {
+      commit(SHOW_TABM);
     },
 
     [CLICK]({ commit }) {
-      commit(CLICKM)
+      commit(CLICKM);
     },
     [SCROLL]({ commit }) {
-      commit(CLICKM)
+      commit(CLICKM);
     },
+  },
+
+  getters: {
+    categoriesIds(state) {
+      return (state.categoriesIds);
     },
-  
-    getters: {
-        categoriesIds(state){
-          return(state.categoriesIds)
-        },
-        showProductSeries(state){
-          return(state.showProductSeries)
-        },
-        getscroll(state){
-          return(state.showProductSeries)
-        }
+    categoryCategoriesIds(state) {
+      return state.categoryCategoriesIds; // Neu hinzugefügt für Kategorien
     },
-  };
-  
+    showProductSeries(state) {
+      return (state.showProductSeries);
+    },
+    getscroll(state) {
+      return (state.showProductSeries);
+    },
+  },
+};
