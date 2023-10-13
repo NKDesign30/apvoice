e0b3e71c1bd45abf5c3cf23fb7d2ae0b8edaf187
NkDesign <70699589+NKDesign30@users.noreply.github.com>
2023-10-13 11:45:39 +0200

    Update SurveyRatingQuestion.vue

--- Frontend/src/components/survey/SurveyRatingQuestion.vue
+++ Frontend/src/components/survey/SurveyRatingQuestion.vue
@@ -19,17 +19,36 @@
 
     <div
       ref="ratingOptions"
-      class="inline-flex flex-wrap justify-center text-center"
+      class="flex flex-wrap justify-center text-center flex-col"
     >
       <div
         v-for="item in filteredItems"
         :key="item.id"
         class="flex flex-col flex-wrap justify-center flex-auto mt-10"
+        style="display: flex; flex-direction: column;align-content: center;"
       >
+        <div
+          v-if="item.image"
+          class="relative px-1 py-2 mb-2 rounded-lg tablet:px-10"
+          style="min-height:250px;width: 420px;"
+        >
+          <div class="absolute inset-0 w-full h-40 h-full overflow-hidden">
+            <img
+              v-if="item.image"
+              :src="item.image"
+              alt="Bild für das Rating-Item"
+              class="object-scale-down object-top w-full h-full ml-auto"
+            >
+          </div>
+        </div>
+
+
+        <!-- Hier fügen Sie das img Tag hinzu -->
         <h5
           class="mb-6 text-xl text-gray-900"
           v-html="$options.filters.formatContent(item.headline)"
         />
+
         <div class="flex justify-center flex-auto -mx-2">
           <!-- eslint-disable max-len -->
           <div
@@ -119,44 +138,131 @@ export default {
       default: 1,
     },
   },
-
   data() {
     return {
       selectedOption: '',
+      lastFilteredItems: null,
+      lastDynamicFilterItems: null,
     };
   },
+
+
   computed: {
     filteredItems() {
-      console.log('Dynamic filter:', this.dynamic_filter);
-      console.log('Value:', this.value);
+      console.log('Aktueller Vuex surveys State in filteredItems:', this.$store.state.surveys);
 
-      // Wenn dynamic_filter nicht aktiviert ist, oder keine Antworten vorhanden sind, oder wir uns im ersten Kapitel befinden, geben Sie alle Elemente zurück.
-      if (!this.dynamic_filter || !this.value || this.value.length === 0 || this.currentChapter === 1) {
+      // Wenn wir uns im ersten Kapitel befinden, geben Sie alle Elemente zurück.
+      if (this.currentChapter === 1) {
+        this.lastFilteredItems = this.items;
+        this.lastDynamicFilterItems = null;
+        console.log('First Chapter - lastFilteredItems:', this.lastFilteredItems);
         return this.items;
       }
 
-      // Filtern Sie nur die IDs, die mit dem Wert "1" bewertet wurden
-      const answeredIdsWithRating1 = this.value
-        .filter(v => v.value === '1')
-        .map(v => v.ratingId);
-      console.log('Answered IDs with rating 1:', answeredIdsWithRating1);
+      // Zugriff auf den gespeicherten Umfragezustand im Vuex Store
+      const surveyState = this.$store.state.surveys;
 
-      // Filtern Sie die Elemente, die nicht in den IDs von Schritt 2 enthalten sind
-      const filtered = this.items.filter(item => !answeredIdsWithRating1.includes(item.id));
-      console.log('Filtered items:', filtered);
+      // Überprüfen Sie, ob surveyState.surveys existiert und ein Array ist
+      if (surveyState && Array.isArray(surveyState.surveys)) {
+      // Extrahieren Sie die Headlines der Items, die mit "1" bewertet wurden
+        const answeredHeadlinesWithRating1 = [];
+        surveyState.surveys.forEach(survey => {
+          survey.chapters.forEach((chapter, chapterIndex) => {
+          // Nur Antworten aus vorherigen Kapiteln berücksichtigen
+            if (chapterIndex < this.currentChapter - 1) {
+              chapter.questions.forEach(question => {
+                if (question.dynamic_filter && question.value && Array.isArray(question.value)) {
+                  question.value.forEach(answer => {
+                    if (answer.value === '1') {
+                      const innerItem = question.items.find(item => item.id === answer.ratingId);
+                      if (innerItem) {
+                        answeredHeadlinesWithRating1.push(innerItem.headline);
+                      }
+                    }
+                  });
+                }
+              });
+            }
+          });
+        });
 
-      return filtered;
+        // Wenn dynamic_filter für das aktuelle Kapitel aktiviert ist oder deaktiviert ist, filtern Sie die Elemente
+        if (this.dynamic_filter || (!this.dynamic_filter && this.currentChapter > 1)) {
+          const filtered = this.items.filter(item => !answeredHeadlinesWithRating1.includes(item.headline));
+          this.lastFilteredItems = filtered; // Speichern der gefilterten Items
+          this.lastDynamicFilterItems = filtered; // Aktualisieren von lastDynamicFilterItems
+          console.log('Dynamic Filter Active - lastFilteredItems:', this.lastFilteredItems);
+          console.log('Updated lastDynamicFilterItems:', this.lastDynamicFilterItems);
+          return filtered;
+        }
+      }
+
+      // Wenn dynamic_filter für das aktuelle Kapitel nicht aktiviert ist, verwenden Sie lastDynamicFilterItems
+      if (!this.dynamic_filter) {
+        console.log('Using lastDynamicFilterItems:', this.lastDynamicFilterItems);
+        return this.lastDynamicFilterItems || this.items;
+      }
+
+      // Wenn surveyState.surveys nicht existiert oder kein Array ist, geben Sie die zuletzt gefilterten Items zurück
+      console.log('Using lastFilteredItems:', this.lastFilteredItems);
+      return this.lastFilteredItems || this.items;
     },
   },
 
 
-  mounted() {
-    console.log('dynamic_filter value:', this.dynamic_filter);
-    if (this.isCluster) this.$refs.ratingHeader.style.maxWidth = `${this.$refs.ratingOptions.getBoundingClientRect().width}px`;
+  watch: {
+    currentChapter() {
+      console.log('Aktueller Vuex State beim Wechsel des Kapitels:', this.$store.state);
+      // Aktualisieren Sie lastDynamicFilterItems immer, wenn sich die gefilterten Elemente ändern
+      this.lastDynamicFilterItems = this.filteredItems;
+      console.log('Updated lastDynamicFilterItems on chapter change:', this.lastDynamicFilterItems);
+    },
   },
 
-};
 
+  methods: {
+    updateLastFilteredItems() {
+      console.log('updateLastFilteredItems called');
+      if (this.currentChapter === 1) {
+        this.lastFilteredItems = this.items;
+      } else if (this.dynamic_filter) {
+        const surveyState = this.$store.state.surveys;
+        if (surveyState && Array.isArray(surveyState.surveys)) {
+          const answeredHeadlinesWithRating1 = [];
+          surveyState.surveys.forEach(survey => {
+            survey.chapters.forEach((chapter, chapterIndex) => {
+              if (chapterIndex < this.currentChapter - 1) {
+                chapter.questions.forEach(question => {
+                  if (question.dynamic_filter && question.value && Array.isArray(question.value)) {
+                    question.value.forEach(answer => {
+                      if (answer.value === '1') {
+                        const innerItem = question.items.find(item => item.id === answer.ratingId);
+                        if (innerItem) {
+                          answeredHeadlinesWithRating1.push(innerItem.headline);
+                        }
+                      }
+                    });
+                  }
+                });
+              }
+            });
+          });
+          this.lastFilteredItems = this.items.filter(item => !answeredHeadlinesWithRating1.includes(item.headline));
+        }
+      }
+    },
+  },
+
+  mounted() {
+    console.log('Initial lastDynamicFilterItems:', this.lastDynamicFilterItems);
+    console.log('Vuex State: ', this.$store.state);
+    console.log('dynamic_filter value:', this.dynamic_filter);
+    console.log('currentChapter value:', this.currentChapter);
+    console.log('value:', this.value);
+    console.log('items:', this.items);
+    if (this.isCluster) this.$refs.ratingHeader.style.maxWidth = `${this.$refs.ratingOptions.getBoundingClientRect().width}px`;
+  },
+};
 </script>
 
 <style lang="scss" scoped>
@@ -186,7 +292,9 @@ export default {
       @apply border-gray-900;
     }
   }
-
+  .object-cover {
+    object-fit: scale-down!important;
+}
   &-number {
     @apply absolute;
     @apply bg-white;
